a656d0ad29dd0a9db525a52c5d9a016751e1e750
sinoc <gzroc@qq.com>
2021-10-25 22:20:31 +0800

    sinoc 2

--- config.h
+++ config.h
@@ -11,14 +11,14 @@ static const int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "monospace:size=14" };
+static const char *fonts[]          = {"monospace:size=14" };
 static const char dmenufont[]       = "monospace:size=14";
-static const char col_gray1[]       = "#ff6f24";
+static const char col_gray1[]       = "#434343";
 /*static const char col_gray1[]       = "#222222";*/
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 /*static const char col_gray4[]       = "#eeeeee";*/
-static const char col_gray4[]       = "#000000";
+static const char col_gray4[]       = "#ffffff";
 /*static const char col_cyan[]        = "#005577";*/
 static const char col_cyan[]        = "#6e0b0b";
 static const unsigned int baralpha = 0xd0;
