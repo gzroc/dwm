7c776068f9ecb167611e544e9b3e9b47bb4c3938
KEN <ken8882@outlook.com>
2022-10-24 20:14:05 +0800

    Modify dmenu'font

--- config.def.h
+++ config.def.h
@@ -15,7 +15,7 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = {
   "JetBrainsMono Nerd Font Mono:size=14","JoyPixels:size=14:antialias=true:autohint=true" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font Mono:size=14";
+static const char dmenufont[]       = "Sauce Code Pro Nerd Font Mono:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
